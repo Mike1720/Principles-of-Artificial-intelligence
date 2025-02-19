89a72530fb61c88918d2fd9a54ec949491388cab
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-02-18 21:03:58 -0600

    Class: 01-hello-world

---
+++ PRINCIPIOS_INTELIGENCIA_ARTIFICIAL/01_Python_cero/02_Introduccion_Python/01-holamundo.py
@@ -0,0 +1,22 @@
+# HOLA MUNDO EN PYTHON
+
+print("Hola mundo")
+
+
+# COMENTARIOS EN PYTHON
+
+# Esto es un comentario de una sola línea, se puede escribir en cualquier parte del código y no afecta la ejecución del programa
+print("Texto antes del comentario")
+# Esto es un comentario de una sola línea
+print("Texto después del comentario")
+
+
+print("Texto antes del comentario largo")
+'''
+Esto es un comentario de varias líneas, se puede escribir en cualquier parte del código y no afecta la ejecución del programa
+esto es parte del comentario
+
+
+esto es parte del comentario
+'''
+print("Texto después del comentario largo")
