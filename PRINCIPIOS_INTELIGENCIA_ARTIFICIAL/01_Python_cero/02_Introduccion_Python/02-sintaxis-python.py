0624f4556553e9905f75956510b61cdc780b4db5
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2025-02-18 21:04:39 -0600

    Class: 02-sintaxis

---
+++ PRINCIPIOS_INTELIGENCIA_ARTIFICIAL/01_Python_cero/02_Introduccion_Python/02-sintaxis-python.py
@@ -0,0 +1,13 @@
+# SINTAXIS DE PYTHON
+
+# Reglas que se deben seguir al escribir código en  para que sea válido y pueda ser interpretado correctamente
+
+# INDENTACIÓN
+'''
+ La indentación es el "espacio en blanco" llamada tabulación o sangría añ comienzo de una línea de código.
+ El scope/alcance de una estructura no se delimitará por llaves como en otros lenguajes de programación, sino por la indentación.
+'''
+
+# ------------------------------------------
+for i in range(5):
+    print(i)
